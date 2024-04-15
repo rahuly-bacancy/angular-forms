91e287cb19035f4f8c571873a983551461c28a25
RYadav40 <Rahul.Yadav40@T-Mobile.com>
2024-04-15 11:45:03 +0530

    Added required form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,62 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lecture-20';
+  options = [
+    {
+      'value': 'option_1',
+      'label': 'Option 1' 
+    },
+    {
+      'value': 'option_2',
+      'label': 'Option 2' 
+    }
+  ];
+  selectedOptions = "music";
+  selectedGender = "male";
+  userName = 'adminUser';
+
+  @ViewChild('formValue') fromData!: NgForm;
+  
+  suggestUserName() {
+    // this.fromData.setValue({
+    //   'userData': {
+    //     'firstName': "test",
+    //     'lastName': "test",
+    //     'email': 'test@test',
+    //     'username': this.userName,
+    //     'phone': 84012
+    //   },
+    //   'address': 'test',
+    //   'gender': 'male',
+    //   'hobbies': 'reading',
+    //   'checkbox1': true,
+    //   'checkbox2':false
+    // })
+    // this.fromData.setValue({
+    //   'userData': {
+    //     'firstName': "",
+    //     'lastName': "",
+    //     'email': '',
+    //     'username': this.userName,
+    //     'phone': ''
+    //   },
+    //   'address': '',
+    //   'gender': '',
+    //   'hobbies': '',
+    //   'checkbox1': false,
+    //   'checkbox2':false
+    // })
+   this.fromData.form.patchValue({
+    'userData': {
+      'username': this.userName
+    }
+   })
+  }
+
+  submitForm(formValue: NgForm){
+    console.log(this.fromData.form);
+    console.log("---------test----------")
+    this.fromData.reset();
+    console.log(this.fromData.form)
+  }
 }

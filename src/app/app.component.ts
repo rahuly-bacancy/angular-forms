a6b611914553c30b01d8e7b27b995fc66856c588
RYadav40 <Rahul.Yadav40@T-Mobile.com>
2024-04-18 15:17:50 +0530

    Demo done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lecture-20';
+  defaultgender = "female";
+  @ViewChild('formData') formValue!: NgForm;
+
+  submitForm(formData: NgForm) {
+    //console.log(this.formValue);
+    formData.form.reset()
+    console.log(formData.form);
+  }
+
+  patchValue() {
+    this.formValue.form.patchValue({
+      "userData": {
+          "email": "ragy"
+      }
+    });
+  }
 }

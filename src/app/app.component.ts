038280bfbd31a9c8be8837257136f6ce883dffbe
RYadav40 <Rahul.Yadav40@T-Mobile.com>
2024-04-18 10:57:15 +0530

    Final code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lecture-20';
+  defaultName= "Rahul";
+  @ViewChild('formData') formValue!: NgForm;
+
+  submitForm(formData:NgForm) {
+    formData.form.reset();
+    console.log(formData);
+  }
+
+  setValue() {
+    this.formValue.setValue({
+      'userData': {
+        'name': 'Test 123',
+        'email': 'rahul@test.com',
+        'phone': '12356'
+      },
+      'gender': 'female',
+      'hobbies': 'reading',
+      'remember_me': true
+    })
+  }
+
+  patchValue() {
+    this.formValue.form.patchValue({
+      'hobbies': 'reading'
+    })
+  }
 }

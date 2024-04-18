c617100013bc2e98704839638fabba3cb71b5838
RYadav40 <Rahul.Yadav40@T-Mobile.com>
2024-04-18 09:38:09 +0530

    Final change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lecture-20';
+
+  @ViewChild('formData') formValue!: NgForm;
+  defaultname = 'Rahul';
+  gender = 'male';
+  
+  submitForm(formData:NgForm) {
+    console.log(formData.form);
+  }
+
+  setValue() {
+    this.formValue.setValue({
+      'userData' : {
+        'name': 'Rahul Yadav',
+        'email': 'rahul.yadav@bacancy.com',
+        'phone': 878564
+      },
+      'gender': 'male',
+      'hobbies': 'reading',
+      'remember_me': true
+    });
+  }
+
+  patchValue() {
+    this.formValue.form.patchValue({
+      'userData' : {
+        'name': '',
+        'email': 'rahul.yadav@bacancy.com',
+        'phone': 878564
+      }
+    });
+  }
 }
